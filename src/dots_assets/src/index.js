170ae0384b817d1b941495f2d1cf871b5628dd29
Quint Daenen <me@di-wu.be>
2021-09-22 09:39:56 +0200

    Simplify p5 setup.

--- src/dots_assets/src/index.js
+++ src/dots_assets/src/index.js
@@ -6,7 +6,7 @@ document.getElementById("stoic").addEventListener("click", async () => {
   StoicIdentity.load().then(async identity => {
     if (identity !== false) {
       StoicIdentity.disconnect();
-      document.getElementById("stoic").innerText = "Stoic"
+      document.getElementById("stoic").innerText = "Login with Stoic"
       return;
     }
     identity = await StoicIdentity.connect();
@@ -24,3 +24,31 @@ document.getElementById("stoic").addEventListener("click", async () => {
     document.getElementById("stoic").innerText = identity.getPrincipal().toText();
   })
 });
+
+const sketchContainer = document.getElementById("sketch-container");
+
+const sketch = (p) => {
+  const position = {
+    x : 0,
+    y : 0
+  };
+
+  p.setup = () => {
+    const containerPos = sketchContainer.getBoundingClientRect();
+    const cnv = p.createCanvas(containerPos.width, containerPos.height); //the canvas!
+
+    cnv.mousePressed(() => {
+      position.x = p.mouseX;
+      position.y = p.mouseY;
+    });
+    p.fill(255);
+    p.frameRate(30);
+  };
+
+  p.draw = () => {
+    p.background(0);
+    p.circle(position.x, position.y, 10)
+  };
+};
+
+new p5(sketch, sketchContainer);
