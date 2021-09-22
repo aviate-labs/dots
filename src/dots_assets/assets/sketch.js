6a8df9e551c1a455b125de9f576e178098e7bea5
Quint Daenen <me@di-wu.be>
2021-09-22 08:50:35 +0200

    Add snake.

--- src/dots_assets/assets/sketch.js
+++ src/dots_assets/assets/sketch.js
@@ -1,8 +1,131 @@
+
+// the snake is divided into small segments, which are drawn and edited on each 'draw' call
+let numSegments = 10;
+let direction = 'right';
+
+const xStart = 0; //starting x coordinate for snake
+const yStart = 250; //starting y coordinate for snake
+const diff = 10;
+
+let xCor = [];
+let yCor = [];
+
+let xFruit = 0;
+let yFruit = 0;
+let scoreElem;
+
 function setup() {
-    createCanvas(400, 400);
+    scoreElem = createDiv('Score = 0');
+    scoreElem.position(20, 20);
+    scoreElem.id = 'score';
+
+    createCanvas(500, 500);
+    frameRate(15);
+    stroke(255);
+    strokeWeight(10);
+    updateFruitCoordinates();
+
+    for (let i = 0; i < numSegments; i++) {
+        xCor.push(xStart + i * diff);
+        yCor.push(yStart);
+    }
 }
-  
+
 function draw() {
-    background(220);
-    ellipse(50,50,80,80);
+    background(0);
+    for (let i = 0; i < numSegments - 1; i++) {
+        line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
+    }
+    updateSnakeCoordinates();
+    checkGameStatus();
+    checkForFruit();
+}
+
+function updateSnakeCoordinates() {
+    for (let i = 0; i < numSegments - 1; i++) {
+        xCor[i] = xCor[i + 1];
+        yCor[i] = yCor[i + 1];
+    }
+    switch (direction) {
+        case 'right':
+            xCor[numSegments - 1] = xCor[numSegments - 2] + diff;
+            yCor[numSegments - 1] = yCor[numSegments - 2];
+            break;
+        case 'up':
+            xCor[numSegments - 1] = xCor[numSegments - 2];
+            yCor[numSegments - 1] = yCor[numSegments - 2] - diff;
+            break;
+        case 'left':
+            xCor[numSegments - 1] = xCor[numSegments - 2] - diff;
+            yCor[numSegments - 1] = yCor[numSegments - 2];
+            break;
+        case 'down':
+            xCor[numSegments - 1] = xCor[numSegments - 2];
+            yCor[numSegments - 1] = yCor[numSegments - 2] + diff;
+            break;
+    }
+}
+
+function checkGameStatus() {
+    console.log("IOE")
+    if (
+        xCor[xCor.length - 1] > width ||
+        xCor[xCor.length - 1] < 0 ||
+        yCor[yCor.length - 1] > height ||
+        yCor[yCor.length - 1] < 0 ||
+        checkSnakeCollision()
+    ) {
+        noLoop();
+        const scoreVal = parseInt(scoreElem.html().substring(8));
+        scoreElem.html('Game ended! Your score was : ' + scoreVal);
+    }
+}
+
+function checkSnakeCollision() {
+    const snakeHeadX = xCor[xCor.length - 1];
+    const snakeHeadY = yCor[yCor.length - 1];
+    for (let i = 0; i < xCor.length - 1; i++) {
+        if (xCor[i] === snakeHeadX && yCor[i] === snakeHeadY) {
+            return true;
+        }
+    }
+}
+
+function checkForFruit() {
+    point(xFruit, yFruit);
+    if (xCor[xCor.length - 1] === xFruit && yCor[yCor.length - 1] === yFruit) {
+        const prevScore = parseInt(scoreElem.html().substring(8));
+        scoreElem.html('Score = ' + (prevScore + 1));
+        xCor.unshift(xCor[0]);
+        yCor.unshift(yCor[0]);
+        numSegments++;
+        updateFruitCoordinates();
+    }
+}
+
+function updateFruitCoordinates() {
+    xFruit = floor(random(10, (width - 100) / 10)) * 10;
+    yFruit = floor(random(10, (height - 100) / 10)) * 10;
+}
+
+function keyPressed() {
+    // https://keycode.info/
+    switch (keyCode) {
+        case 37, 65:
+            if (direction !== 'right') {
+                direction = 'left';
+            }; break;
+        case 39, 68:
+            if (direction !== 'left') {
+                direction = 'right';
+            }; break;
+        case 38, 87:
+            if (direction !== 'down') {
+                direction = 'up';
+            }; break;
+        case 40, 83:
+            if (direction !== 'up') {
+                direction = 'down';
+            }; break;
+    }
 }
